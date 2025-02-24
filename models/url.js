f096854dc9cd265072b6dc3f04beedac6fa3e9c9
Akshad Gawde <gawdeakshad@gmail.com>
2025-02-24 18:33:22 +0530

    test

--- models/url.js
+++ models/url.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 
 const urlSchema = new mongoose.Schema(
   {
-    shortId: { type: String, required: true, unique: true },
-    redirectedUrl: {
-      type: String,
-      required: true,
-    },
-    visitHistory: [{ timestapm: { type: Number } }],
+    shortid: { type: String, required: true, unique: true },
+    redirectUrl: { type: String, required: true },
+    visitHistory: [{ timestamp: { type: Number } }],
   },
-  { timestamps: true }
+  { timestamp: true }
 );
 
 const URL = mongoose.model("URL", urlSchema);
